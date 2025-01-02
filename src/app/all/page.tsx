579757d50105228c0eb9cc500361e5fe8030d45b
dspedroborges <xpedrostewart@gmail.com>
2025-01-01 22:36:01 -0300

    first commit

---
+++ src/app/all/page.tsx
@@ -0,0 +1,229 @@
+"use client";
+
+import { useRouter, useSearchParams } from 'next/navigation'
+import { Suspense, useEffect, useState } from 'react';
+import { BsCheckCircle, BsCircle, BsClipboard2, BsClipboard2Check, BsHouse, BsPlusCircleDotted, BsTrash, BsTrashFill } from 'react-icons/bs';
+import Link from 'next/link';
+
+type Task = { days: string, task: string, done: boolean };
+
+function weekDayToNumber(weekDay: string) {
+    if (weekDay == "all") return -1;
+    const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+    const number = days.indexOf(weekDay.toLowerCase());
+    return number;
+}
+
+function numberToWeekDay(number: number) {
+    if (number == -1) return "all";
+    const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+    return days[number];
+}
+
+export default function Page() {
+    return (
+        <Suspense>
+            <Content />
+        </Suspense>
+    )
+}
+
+function Content() {
+    const searchParams = useSearchParams();
+    let tasks = searchParams.get("tasks");
+    const [currentTasks, setCurrentTasks] = useState<Task[]>();
+    const [addedClipboard, setAddedClipboard] = useState(false);
+    const [showCreateUpdate, setShowCreateUpdate] = useState(false);
+    const [showDelete, setShowDelete] = useState(false);
+    const addToClipboard = () => {
+        navigator.clipboard.writeText(window.location.href);
+    }
+    const router = useRouter();
+
+    useEffect(() => {
+        if (tasks) {
+            const all = tasks.split(">>");
+            const allInObject = all.map(a => {
+                const [days, task, done] = a.split(";");
+                return { days: days, task, done: done == "true" };
+            });
+            setCurrentTasks(allInObject)
+        }
+    }, []);
+
+    const getArrToObjUrl = () => {
+        if (!currentTasks) return "";
+        const arrObjToUrl = currentTasks?.map(ct => {
+            let allElements: any = [];
+            for (const key of Object.keys(ct) as (keyof Task)[]) {
+                allElements.push(ct[key])
+            }
+            return allElements.join(";")
+        })
+
+        return arrObjToUrl?.join(">>");
+    }
+
+    useEffect(() => {
+        if (currentTasks && tasks) router.push(`?tasks=${getArrToObjUrl()}`);
+    }, [currentTasks]);
+
+    return (
+        <main className="p-8">
+            <h1 className="text-white text-4xl text-center my-4">Routine Care</h1>
+            <div className="flex items-center justify-center gap-4">
+                <BsPlusCircleDotted onClick={() => setShowCreateUpdate(true)} className="text-4xl text-white mb-8 hover:scale-90 cursor-pointer" />
+                <Link href={`/?tasks=${getArrToObjUrl()}`}>
+                    <BsHouse className="text-4xl text-white mb-8 hover:scale-90 cursor-pointer" />
+                </Link>
+            </div>            {
+                currentTasks?.map((ct, i) => {
+                    return <div
+                        key={i}
+                        className={`${ct.done ? "bg-purple-900" : "bg-gray-800"} text-white p-2 rounded-xl mb-2 cursor-pointer hover:brightness-125 relative group`}
+                    >
+                        {ct.task}
+                        <span className="bg-black px-2 py-1 mx-4 text-xs rounded-xl">{ct.days.split(",").map(d => numberToWeekDay(Number(d))).join(", ") }</span>
+                        {
+                            showDelete ? (
+                                <BsTrashFill onClick={(e) => {
+                                    e.stopPropagation();
+                                    setCurrentTasks(currentTasks.toSpliced(i, 1));
+                                    setShowDelete(false);
+                                }} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hidden group-hover:inline-block text-red-300" />
+                            ) : (
+                                <BsTrash onClick={(e) => {
+                                    e.stopPropagation();
+                                    setShowDelete(true);
+                                    setTimeout(() => {
+                                        setShowDelete(false);
+                                    }, 3000);
+                                }} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hidden group-hover:inline-block" />
+                            )
+                        }
+                        {ct.done && <BsCheckCircle className="absolute top-1/2 right-4 -translate-y-1/2" />}
+                    </div>
+                })
+            }
+
+            {
+                addedClipboard ? (
+                    <BsClipboard2Check title="URL copied to clipboard" className="fixed bottom-2 right-2 text-green-50 text-4xl hover:scale-95 cursor-pointer" />
+                ) : (
+                    <BsClipboard2 title="Copy URL to clipboard" className="fixed bottom-2 right-2 text-white text-4xl hover:scale-95 cursor-pointer" onClick={() => {
+                        setAddedClipboard(true);
+                        addToClipboard();
+                        setTimeout(() => {
+                            setAddedClipboard(false)
+                        }, 2000);
+                    }} />
+                )
+            }
+
+            {
+                showCreateUpdate && (
+                    <CreateUpdateTask
+                        currentTasks={currentTasks || []}
+                        setCurrentTasks={setCurrentTasks}
+                        setShowCreateUpdate={setShowCreateUpdate}
+                    />
+                )
+            }
+        </main>
+    );
+}
+
+function CreateUpdateTask({ index, currentTasks, setCurrentTasks, setShowCreateUpdate }: { index?: number, currentTasks: Task[], setCurrentTasks: Function, setShowCreateUpdate: Function }) {
+    const handleAction = (formData: FormData) => {
+        const formTask = formData.get("task");
+        let handledDays = (formData.get("days") as string).replaceAll(" ", "").split(",").map(d => weekDayToNumber(d)).join(",");
+
+        if (!formTask || !handledDays) return;
+
+        if (index) {
+            const copy = JSON.parse(JSON.stringify(currentTasks));
+            copy[index] = { ...copy[index], days: handledDays, task: formTask };
+            setCurrentTasks(copy);
+        } else {
+            const newTaskData = {
+                days: handledDays,
+                task: formTask,
+                done: false,
+            }
+            setCurrentTasks([...currentTasks, newTaskData]);
+        }
+        setShowCreateUpdate(false);
+    }
+
+    return (
+        <div className="bg-black bg-opacity-80 fixed top-0 left-0 h-screen w-full flex items-center justify-center p-4 z-50" onClick={() => setShowCreateUpdate(false)}>
+            <form action={handleAction} onClick={(e) => e.stopPropagation()} className="border p-8 text-white rounded-xl">
+                <div>
+                    <label htmlFor="task" className="font-bold mb-2 block my-4">Task:</label>
+                    <input type="text" name="task" id="task" className="p-4 rounded-xl bg-gray-800" autoComplete={"off"} autoCorrect="off" autoCapitalize="on" />
+                </div>
+                <div>
+                    <label htmlFor="days" className="font-bold mb-2 block my-4">Days:</label>
+                    <Combobox items={["all", "sun", "mon", "tue", "wed", "thu", "fri", "sat"]} name={"days"} id={"days"} />
+                </div>
+                <button className="w-full p-4 rounded-full bg-purple-900 text-white hover:bg-purple-800 hover:scale-95 mt-4">{index ? "Atualizar" : "Criar"}</button>
+            </form>
+        </div>
+    )
+}
+
+function Combobox({ items, name, id, onChange }: { items: string[], name: string, id: string, onChange?: Function }) {
+    const [options] = useState<string[]>(items);
+    const [selectedItems, setSelectedItems] = useState<string[]>([]);
+
+    const handleSelect = (item: string) => {
+        if (!selectedItems.includes(item)) {
+            setSelectedItems([...selectedItems, item]);
+        }
+    };
+
+    const handleRemove = (item: string) => {
+        setSelectedItems(selectedItems.filter(selected => selected !== item));
+    };
+
+    useEffect(() => {
+        if (onChange) onChange(selectedItems);
+    }, [selectedItems]);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const inputValues = e.target.value.split(",").map(value => value.trim());
+        const validValues = inputValues.filter(value => options.includes(value));
+        setSelectedItems(validValues);
+    };
+
+    return (
+        <div className="select-none">
+            <input
+                type="text"
+                value={selectedItems.join(", ")}
+                onChange={handleChange}
+                className="hidden"
+                name={name}
+                id={id}
+            />
+            <ul className="border border-dashed rounded p-2 mt-2 max-h-40 overflow-auto">
+                {options.map(option => (
+                    <li
+                        key={option}
+                        onClick={() => {
+                            if (!selectedItems.includes(option)) {
+                                handleSelect(option)
+                            } else {
+                                handleRemove(option)
+                            }
+                        }}
+                        className={`p-2 cursor-pointer hover:bg-gray-800 rounded-xl mb-1 flex items-center gap-2 ${selectedItems.includes(option) ? "bg-purple-950" : ""}`}
+                    >
+                        {selectedItems.includes(option) ? <BsCheckCircle className="text-base" /> : <BsCircle className="text-base" />}
+                        {option}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,102 @@
-import Image from "next/image";
+"use client";
+
+import Link from 'next/link';
+import { useRouter, useSearchParams } from 'next/navigation'
+import { Suspense, useEffect, useState } from 'react';
+import { BsCheckCircle, BsCircle, BsClipboard2, BsClipboard2Check, BsEye, BsPencil, BsPlusCircleDotted, BsTrash, BsTrashFill } from 'react-icons/bs';
+
+type Task = { days: string, task: string, done: boolean };
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <Suspense>
+      <Content/>
+    </Suspense>
+  )
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+function Content() {
+  const searchParams = useSearchParams();
+  let tasks = searchParams.get("tasks");
+  const [currentTasks, setCurrentTasks] = useState<Task[]>();
+  const [addedClipboard, setAddedClipboard] = useState(false);
+  const addToClipboard = () => {
+    navigator.clipboard.writeText(window.location.href);
+  }
+  const router = useRouter();
+
+  useEffect(() => {
+    if (tasks) {
+      const all = tasks.split(">>");
+      const allInObject = all.map(a => {
+        const [days, task, done] = a.split(";");
+        return { days: days, task, done: done == "true" };
+      });
+      console.log({allInObject})
+      setCurrentTasks(allInObject)
+    }
+  }, []);
+
+  const getArrToObjUrl = () => {
+    if (!currentTasks) return "";
+    const arrObjToUrl = currentTasks?.map(ct => {
+      let allElements: any = [];
+      for (const key of Object.keys(ct) as (keyof Task)[]) {
+        allElements.push(ct[key])
+      }
+      return allElements.join(";")
+    })
+
+    return arrObjToUrl?.join(">>");
+  }
+
+  useEffect(() => {
+    if (currentTasks && tasks) router.push(`?tasks=${getArrToObjUrl()}`);
+  }, [currentTasks]);
+
+  const toggleTaskDone = (index: number) => {
+    const copy = JSON.parse(JSON.stringify(currentTasks));
+    copy[index] = { ...copy[index], done: !copy[index].done };
+    setCurrentTasks(copy);
+  }
+
+  return (
+    <main className="p-8">
+      <h1 className="text-white text-4xl text-center my-4">Routine Care</h1>
+      <div className="flex items-center justify-center gap-4">
+        <Link href={`/all?tasks=${getArrToObjUrl()}`}>
+          <BsPencil className="text-4xl text-white mb-8 hover:scale-90 cursor-pointer" />
+        </Link>
+      </div>
+      {
+        currentTasks?.map((ct, i) => {
+          const currentDay = new Date().getDay();
+          if (ct.days === "-1" || ct.days.split(",").includes(String(currentDay))) {
+            return <div
+              key={i}
+              className={`${ct.done ? "bg-purple-900" : "bg-gray-800"} text-white p-2 rounded-xl mb-2 cursor-pointer hover:brightness-125 relative group`}
+              onClick={() => toggleTaskDone(i)}
+            >
+              {ct.task}
+              {ct.done && <BsCheckCircle className="absolute top-1/2 right-4 -translate-y-1/2" />}
+            </div>
+          }
+        })
+      }
+
+      {
+        addedClipboard ? (
+          <BsClipboard2Check title="URL copied to clipboard" className="fixed bottom-2 right-2 text-green-50 text-4xl hover:scale-95 cursor-pointer" />
+        ) : (
+          <BsClipboard2 title="Copy URL to clipboard" className="fixed bottom-2 right-2 text-white text-4xl hover:scale-95 cursor-pointer" onClick={() => {
+            setAddedClipboard(true);
+            addToClipboard();
+            setTimeout(() => {
+              setAddedClipboard(false)
+            }, 2000);
+          }} />
+        )
+      }
+    </main>
   );
 }
