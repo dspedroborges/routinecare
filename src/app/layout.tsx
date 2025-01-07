3cae1b2fc6d9276186da64ff74c7381bfdadb984
dspedroborges <xpedrostewart@gmail.com>
2025-01-06 21:56:29 -0300

    gradient to r

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-b min-h-screen from-black to-purple-950`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-r min-h-screen from-black to-purple-950`}
       >
         {children}
       </body>
